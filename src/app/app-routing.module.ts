e891c6356d98e74c5927a74ff8298124ed5a9df5
Ashraf Uddin <ahafuddn22@gmail.com>
2020-06-14 14:37:32 +0200

    add routing and base app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
+import { EstablishmentDetailsComponent } from './components/establishment-details/establishment-details.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { EstablishmentsComponent } from './components/establishments/establishments.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', component: EstablishmentsComponent
+  },
+  {
+    path: 'establishment/:establishmentId', component: EstablishmentDetailsComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,38 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { EstablishmentCardComponent } from './components/establishments/establishment-card/establishment-card.component';
+import { EstablishmentsComponent } from './components/establishments/establishments.component';
+import { EstablishmentDetailsComponent } from './components/establishment-details/establishment-details.component';
+import { EstablishmentService } from './components/establishments/establishment.service';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NgxMaskModule, optionsConfig } from 'ngx-mask-2';
+import { LocalStorageService } from './components/establishments/local-storage.service';
+import { MessageComponent } from './components/message/message.component';
+
+
+const maskConfig: optionsConfig = {};
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    EstablishmentCardComponent,
+    EstablishmentsComponent,
+    EstablishmentDetailsComponent,
+    MessageComponent,
   ],
   imports: [
+    HttpClientModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    NgxMaskModule.forRoot(maskConfig)
   ],
-  providers: [],
+  providers: [EstablishmentService, LocalStorageService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
